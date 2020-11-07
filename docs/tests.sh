fe9906b47d3e457a44f2dbcd8bc84e4c70c12381
Markus Peura <Markus.Peura@iki.fi>
2020-11-07 17:08:15 +0200

    Clean

--- docs/tests.sh
+++
@@ -1,3 +0,0 @@
-python3 -m nutshell.nutshell --move nut-move.png  -m   -p 201708121600_radar.rack.comp_CONF=RAVAKE_SITES=fikor.png
-python3 -m nutshell.nutshell --copy nut-copy.png  -dm   -p 201708121600_radar.rack.comp_CONF=RAVAKE_SITES=fikor.png
-python3 -m nutshell.nutshell --link --latest -m   -p 201708121600_radar.rack.comp_CONF=RAVAKE_SITES=fikor.png
