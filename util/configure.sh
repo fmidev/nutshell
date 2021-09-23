b3f315b23726b40fb3a1b24406fa40dc14ae99dc
Markus Peura <Markus.Peura@iki.fi>
2021-09-23 22:53:42 +0300

    PATH2

--- util/configure.sh
+++ util/configure.sh
@@ -141,7 +141,7 @@ ask_variable HTTP_PORT "8088" "Port for HTTP server (Python)."
 ask_variable HTTP_PREFIX "/nutshell" "URL prefix (with leading '/' but without trailing '/')"
 check_dir_syntax HTTP_PREFIX
 
-ask_variable PATH "/var/bin:/opt/bin" "Additional command search path appended to $PATH: ."
+ask_variable PATH2 "" "Additional command search path appended to $PATH: ."
 
 echo "Created $CONF_FILE with contents:"
 echo "---------------------------------"
